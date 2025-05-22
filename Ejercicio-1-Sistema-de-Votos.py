bd81d28da42032f2d9443c9e2534234d8ad95734
Maximo <maximors2110@gmail.com>
2025-05-22 00:19:41 -0300

    Programa Finalizado. Se imprime por pantalla el ganador y los votos totales del mismo.

--- Ejercicio-1-Sistema-de-Votos.py
+++ Ejercicio-1-Sistema-de-Votos.py
@@ -2,6 +2,7 @@
 
 localidades = [['' for i in range(3)] for j in range(5)]
 
+#Sistema de agregado de votos
 i = 0
 while i != 5:
     print(f"--LOCALIDAD {i+1}--")
@@ -12,5 +13,29 @@ while i != 5:
     else:
         print("La cantidad de votantes ingresados es mayor a 300. Por favor, respete la condición")
 
+
+#Impresión de Resultados de votos
+print("\n")
+cont = 1
 for i in localidades:
-    print(i)
+    print(f"--LOCALIDAD {cont}--", i)
+    cont += 1
+
+#Ganador
+win_1 = 0
+win_2 = 0
+win_3 = 0
+
+for i in range(5):
+    win_1 += localidades[i][0]
+    win_2 += localidades[i][1]
+    win_3 += localidades[i][2]
+
+if win_1 > win_2 and win_1 > win_3:
+    print(f"El partido 1 es el ganador con {win_1} votos")
+elif win_2 > win_1 and win_2 > win_3:
+    print(f"El partido 2 es el ganador con {win_2} votos")
+elif win_3 > win_1 and win_3 > win_2:
+    print(f"El partido 3 es el ganador con {win_3} votos")
+else:
+    print("Hay un empate entre partidos")
